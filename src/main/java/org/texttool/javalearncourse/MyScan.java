db123ef77997a23e54c2324209f41273237b5ef0
Dmitry Savkin <dmitry.savkin.info@gmail.com>
2021-05-01 13:44:51 +0200

    adds PropertySource Annotation

--- src/main/java/org/texttool/javalearncourse/MyScan.java
+++ src/main/java/org/texttool/javalearncourse/MyScan.java
@@ -1,7 +1,13 @@
 package org.texttool.javalearncourse;
 
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.annotation.PropertySource;
 
+@Configuration
 @ComponentScan(basePackages = "org.texttool.javalearncourse")
+//@ImportResource(locations = {"classpath:values.properties"})
+@PropertySource(name="myProperties", value = "values.properties")
 public class MyScan {
 }
